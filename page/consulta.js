a0206e0691c5e275b96c59553b2c6179d82482f3
danielsimonebeira <dante_beira@hotmail.com>
2020-04-26 23:17:50 -0300

    ajusta package.json e consulta.js

--- page/consulta.js
+++ page/consulta.js
@@ -1,10 +1,3 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/* global response */
-
 const axios = require('axios');
 const chai = require('chai');
 const { URL_API, ENDPOINT } = require('../constants/acesso.js')
